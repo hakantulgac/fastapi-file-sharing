444ecab6f4e884e8563e3ce6cd77de6ac095276b
Hakan Tulgaç <hakantulgac@gmail.com>
2025-11-07 13:00:41 +0300

    create routers and file upload

---
+++ src/routers/channels.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from src.models import Channel
+from src.db import get_db
+
+router = APIRouter(
+    prefix="/channels",
+    tags=["channels"]
+)
+
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_channel(db: Session = Depends(get_db)):
+    try:
+        new_channel = Channel(number_of_files=0, size_of_files=0)
+        db.add(new_channel)
+        db.commit()
+        db.refresh(new_channel)
+        return new_channel
+    except Exception:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server error."
+        )
+
+
+@router.get("")
+async def get_channels(db: Session = Depends(get_db)):
+    try:
+        channels = db.query(Channel).all()
+        return channels
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server error."
+        )
+
+@router.get("/{channel_id}")
+async def get_channel_by_id(channel_id: str, db: Session = Depends(get_db)):
+    try:
+        channel = db.query(Channel).filter(Channel.id == channel_id).first()
+
+        if not channel:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Channel not found."
+            )
+
+        return channel
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server error."
+        )
+
+@router.delete("/{channel_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_channel(channel_id: str, db: Session = Depends(get_db)):
+    try:
+        channel_to_delete = db.query(Channel).filter(Channel.id == channel_id).first()
+
+        if not channel_to_delete:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Channel not found."
+            )
+
+        db.delete(channel_to_delete)
+        db.commit()
+        return
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server error."
+        )
---
+++ src/routers/files.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter, UploadFile, File, status, HTTPException, Depends
+from sqlalchemy.orm import Session
+from pathlib import Path
+import shutil
+from src.db import get_db
+from src.models import Channel
+
+BASE_TEMP_DIR = Path("temp")
+router = APIRouter(
+    prefix="/files",
+    tags=["files"]
+)
+
+@router.post("upload/{channel_id}", status_code=status.HTTP_201_CREATED)
+async def upload_file(
+        channel_id: str,
+        file: UploadFile = File(...),
+        db: Session = Depends(get_db)
+):
+    upload_dir = BASE_TEMP_DIR / channel_id
+    upload_dir.mkdir(parents=True, exist_ok=True)
+
+    file_name = file.filename
+    file_size = file.size
+    file_content_type = file.content_type
+    file_location = upload_dir / file_name
+
+    try:
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        size_of_files = 0
+        number_of_files = 0
+
+        for item in upload_dir.glob('*'):
+            if item.is_file():
+                number_of_files += 1
+                size_of_files += item.stat().st_size
+
+        db.query(Channel).filter(Channel.id == channel_id).update({
+            "number_of_files": number_of_files,
+            "size_of_files": size_of_files
+        })
+        db.commit()
+
+        return {
+            "message": "Files uploaded successfully",
+            "file_name": file_name,
+            "file_size": file_size,
+            "file_content_type": file_content_type,
+        }
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not save file"
+        )
